85eff76995ef6dd6b65657794b3c70e7300a7f38
DohhhDo <112384233+DohhhDo@users.noreply.github.com>
2025-08-14 16:52:19 +0800

    css

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -19,10 +19,10 @@ export default function BlogLayout({
       <span className="pointer-events-none fixed top-0 block h-[800px] w-full select-none bg-[radial-gradient(103.72%_46.58%_at_50%_0%,rgba(5,5,5,0.045)_0%,rgba(0,0,0,0)_100%)] dark:bg-[radial-gradient(103.72%_46.58%_at_50%_0%,rgba(255,255,255,0.09)_0%,rgba(255,255,255,0)_100%)]" />
 
       {/* Left Sidebar - Entire Page */}
-      <div className="hidden xl:block fixed left-0 top-0 w-64 h-full bg-zinc-50/90 ring-1 ring-zinc-100 dark:bg-zinc-900/80 dark:ring-zinc-400/20 pointer-events-none" />
+      <div className="hidden xl:block fixed left-0 top-0 w-64 h-full bg-zinc-50/90 ring-1 ring-zinc-100 dark:bg-zinc-900/80 dark:ring-zinc-400/20 pointer-events-none animate-[fadeOut_10s_ease-in-out_forwards]" />
       
       {/* Right Sidebar - Entire Page */}
-      <div className="hidden xl:block fixed right-0 top-0 w-64 h-full bg-zinc-50/90 ring-1 ring-zinc-100 dark:bg-zinc-900/80 dark:ring-zinc-400/20 pointer-events-none" />
+      <div className="hidden xl:block fixed right-0 top-0 w-64 h-full bg-zinc-50/90 ring-1 ring-zinc-100 dark:bg-zinc-900/80 dark:ring-zinc-400/20 pointer-events-none animate-[fadeOut_10s_ease-in-out_forwards]" />
 
       <div className="fixed inset-0 flex justify-center sm:px-8">
         <div className="flex w-full max-w-7xl lg:px-8">
