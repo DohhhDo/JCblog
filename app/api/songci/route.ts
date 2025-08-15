dbf27378852c58aeec74834d8f456a66c89c0947
DohhhDo <112384233+DohhhDo@users.noreply.github.com>
2025-08-15 11:28:39 +0800

    Update route.ts

--- app/api/songci/route.ts
+++ app/api/songci/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 
-import { promises as fs } from 'fs'
 import path from 'path'
+import { promises as fs } from 'fs'
 
 export async function GET() {
   try {
