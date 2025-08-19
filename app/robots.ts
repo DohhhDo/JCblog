7878ac51a68acf958338a907bdaa539d8c136361
DohhhDo <112384233+DohhhDo@users.noreply.github.com>
2025-08-19 16:03:12 +0800

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,19 @@
+import { type MetadataRoute } from 'next'
+
+import { url } from '~/lib'
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: url('/sitemap.xml').href,
+    host: url('/').origin,
+  }
+}
+
+export const runtime = 'edge'
+export const revalidate = 60
+
+
