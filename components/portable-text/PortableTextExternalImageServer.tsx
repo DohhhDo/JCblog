20082d886f24a2aa88e77f3ded455bc458bf1239
DohhhDo <112384233+DohhhDo@users.noreply.github.com>
2025-08-20 11:51:02 +0800

    Ai贴图认证

--- components/portable-text/PortableTextExternalImageServer.tsx
+++ components/portable-text/PortableTextExternalImageServer.tsx
@@ -9,18 +9,42 @@ type ExternalImageValue = {
 }
 
 // 透传除 value 外的所有 props，以满足 @portabletext/react 的额外参数要求
-type PTProps<T> = { value: T } & { [key: string]: unknown }
+type PTProps<T> = {
+  value: T
+  index?: number
+  isInline?: boolean
+  renderNode?: unknown
+  [key: string]: unknown
+}
 
 export async function PortableTextExternalImageServer(
   props: PTProps<ExternalImageValue>,
 ) {
-  const { value, ...rest } = props
+  const { value, index = 0, isInline = false, renderNode = () => null, ...rest } = props
   if (value?.alt && value.alt.length > 0) {
-    return <PortableTextExternalImage value={value} {...rest} />
+    return (
+      <PortableTextExternalImage
+        value={value}
+        index={index}
+        isInline={isInline}
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        renderNode={renderNode as never}
+        {...rest}
+      />
+    )
   }
 
   const { alt } = await getAltForImage(value.url, { maxLength: 16 })
-  return <PortableTextExternalImage value={{ ...value, alt }} {...rest} />
+  return (
+    <PortableTextExternalImage
+      value={{ ...value, alt }}
+      index={index}
+      isInline={isInline}
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      renderNode={renderNode as never}
+      {...rest}
+    />
+  )
 }
 
 
--- components/portable-text/PortableTextImageServer.tsx
+++ components/portable-text/PortableTextImageServer.tsx
@@ -10,18 +10,41 @@ type ImageValue = {
   alt?: string
 }
 
-type PTProps<T> = { value: T } & { [key: string]: unknown }
+type PTProps<T> = {
+  value: T
+  index?: number
+  isInline?: boolean
+  renderNode?: unknown
+  [key: string]: unknown
+}
 
-export async function PortableTextImageServer(
-  props: PTProps<ImageValue>,
-) {
-  const { value, ...rest } = props
+export async function PortableTextImageServer(props: PTProps<ImageValue>) {
+  const { value, index = 0, isInline = false, renderNode = () => null, ...rest } = props
   // 如果已有 alt，直接用原组件（客户端）
-  if (value?.alt && value.alt.length > 0) return <PortableTextImage value={value} {...rest} />
+  if (value?.alt && value.alt.length > 0)
+    return (
+      <PortableTextImage
+        value={value}
+        index={index}
+        isInline={isInline}
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        renderNode={renderNode as never}
+        {...rest}
+      />
+    )
 
   // 在服务器端生成 alt，并注入到传给客户端组件的 props
   const { alt } = await getAltForImage(value.url, { maxLength: 16 })
-  return <PortableTextImage value={{ ...value, alt }} {...rest} />
+  return (
+    <PortableTextImage
+      value={{ ...value, alt }}
+      index={index}
+      isInline={isInline}
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      renderNode={renderNode as never}
+      {...rest}
+    />
+  )
 }
 
 
