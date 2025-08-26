2ff94dfcea2fa5e3ef642a5140b8c85a165227e6
DohhhDo <112384233+DohhhDo@users.noreply.github.com>
2025-08-26 14:37:18 +0800

    Update route.ts

--- app/(main)/google-sitemap.xml/route.ts
+++ app/(main)/google-sitemap.xml/route.ts
@@ -2,7 +2,7 @@ import { seo } from '~/lib/seo'
 
 export const revalidate = 60 * 60 // 1 hour
 
-export async function GET() {
+export function GET() {
   const homepage = seo.url.href
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n  <url>\n    <loc>${homepage}</loc>\n  </url>\n</urlset>`
