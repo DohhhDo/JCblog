ab70c0c8c70aa906a2a0b1d1efdcab6e902034de
DohhhDo <112384233+DohhhDo@users.noreply.github.com>
2025-08-26 14:49:37 +0800

    修改网站地图

--- app/(main)/google-sitemap.xml/route.ts
+++ app/(main)/google-sitemap.xml/route.ts
@@ -1,17 +1,7 @@
-import { seo } from '~/lib/seo'
-
 export const revalidate = 60 * 60 // 1 hour
 
 export function GET() {
-  const homepage = seo.url.href
-
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n  <url>\n    <loc>${homepage}</loc>\n  </url>\n</urlset>`
-
-  return new Response(xml, {
-    headers: {
-      'content-type': 'application/xml',
-    },
-  })
+  return new Response('Not found', { status: 404 })
 }
 
 
--- app/robots.ts
+++ app/robots.ts
@@ -8,7 +8,7 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: url('/google-sitemap.xml').href,
+    sitemap: '/google-sitemap.xml',
     host: url('/').origin,
   }
 }
