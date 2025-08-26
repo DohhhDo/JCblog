e3eca8c9e1a12f6fc2053e7d85401d491a2ffe2a
DohhhDo <112384233+DohhhDo@users.noreply.github.com>
2025-08-26 14:54:04 +0800

    站点地图尝试

--- app/robots.ts
+++ app/robots.ts
@@ -1,14 +1,15 @@
-import { type MetadataRoute } from 'next'
-
 import { url } from '~/lib'
 
-export default function robots(): MetadataRoute.Robots {
+export default function robots() {
   return {
     rules: {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: '/google-sitemap.xml',
+    sitemap: [
+      '/google-sitemap.xml',
+      '/sitemap.xml',
+    ],
     host: url('/').origin,
   }
 }
